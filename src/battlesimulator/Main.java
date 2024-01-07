cdc9a2d2f3c69326f8781576e257c6f21aab0db4
vt-mxs <maximusvitor88@gmail.com>
2024-01-07 13:59:02 -0300

    basic structure

---
+++ src/battlesimulator/Main.java
@@ -0,0 +1,16 @@
+package battlesimulator;
+
+/**
+ * @author vt
+ * @version 2.0
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/battlesimulator/classes/Character.java
@@ -0,0 +1,5 @@
+package battlesimulator.classes;
+
+public class Character {
+    
+}
