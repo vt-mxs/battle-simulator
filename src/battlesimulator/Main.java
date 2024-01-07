ebd8b61f8e2267707d56956de688718b4dc106ba
vt-mxs <maximusvitor88@gmail.com>
2024-01-07 14:34:07 -0300

    basic layout

--- src/battlesimulator/Main.java
+++ src/battlesimulator/Main.java
@@ -1,16 +1,49 @@
 package battlesimulator;
-
+import java.util.Scanner;
+import battlesimulator.classes.Hero;
+import battlesimulator.classes.Enemy;
 /**
  * @author vt
  * @version 2.0
  */
 public class Main {
-
+    
+    // método para criação do personagem
+    public static void createHero(Scanner input){
+        String heroName;
+        // recebendo o nome
+        System.out.print("Hero name => ");
+        heroName = input.nextLine();
+        // criação do héroi
+        Hero newHero = new Hero(heroName);
+        System.out.println("Hero was successfully created!");
+        // apresentação do héroi
+        System.out.println(newHero);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // instanciação da classe Scanner para poder receber
+        // os inputs do usuário
+        Scanner input = new Scanner(System.in);
+        
+        // chamada do método para criação do personagem
+        createHero(input);
+        
+        // criação do inimigo
+        Enemy glob = new Enemy("Glob");
+        
+        System.out.println("A foe has appeared in front of you!");
+        System.out.println(glob);
+        
+        System.out.print(
+            """
+            What do you do?
+            1 - Atack
+            2 - Run
+            => """
+        );
     }
-    
 }
