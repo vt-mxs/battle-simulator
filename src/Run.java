ab731ee35efc49116f028b375274d0fce205f16c
vt-mxs <maximusvitor88@gmail.com>
2024-01-09 21:47:14 -0300

    starting over from scratch

--- src/Run.java
+++ src/Run.java
@@ -1,54 +1,17 @@
-package battlesimulator;
-import java.util.Scanner;
-import battlesimulator.classes.Hero;
 import battlesimulator.classes.Enemy;
+import battlesimulator.classes.Hero;
+import java.util.Scanner;
 /**
  * @author vt
  * @version 2.0
  */
-public class Main {
+public class Run {
     
-    // método para criação do personagem
-    private static void createHero(Scanner input){
-        String heroName;
-        // recebendo o nome
-        System.out.print("Hero name => ");
-        heroName = input.nextLine();
-        
-        if (heroName.isEmpty()) {
-            System.out.println("Hero name cannot be null");
-        }
-        else{
-            // o heroi é criado se o nome passado não estiver vázio
-            Hero newHero = new Hero(heroName);
-            System.out.println("Hero was successfully created!");
-            // apresentação do héroi
-            System.out.println(newHero);
-        }
-    }
-    
-    // criação do inimigo
-    private static void createEnemy(String enemyName){
-        // criação do inimigo 
-        Enemy glob = new Enemy(enemyName);
-        
-        // apresentando o inimigo
-        System.out.println("A foe has appeared in front of you!");
-        System.out.println(glob);
-    }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // instanciação da classe Scanner para poder receber
-        // os inputs do usuário
-        Scanner input = new Scanner(System.in);
         
-        // chamada do método para criação do héroi
-        createHero(input);
-        
-        // chamada do método para criação do inimigo
-        createEnemy("Glob");
     }
 }
